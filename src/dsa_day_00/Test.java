3e1eb806f08de191e577e4095c1ac24448c8bac0
Narayan Thakare <narayan111thakare@gmail.com>
2024-02-21 16:35:45 +0530

    first commit

---
+++ src/dsa_day_00/Test.java
@@ -0,0 +1,50 @@
+package dsa_day_00;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Narayan");
+		
+		for(int i=1;i<=10;i++){
+			
+			if(i%2==0){
+				
+				System.out.println("this is even no."+i);
+			}
+			else{
+				System.out.println("this is odd no."+i);
+				
+			}
+			
+			
+		}
+		
+		
+		System.out.println("this is second method for odd no.");
+		
+		
+		for(double i=1;i<=10;i=i+2){
+			
+			System.out.println(i);
+			
+			
+		}
+		
+		
+System.out.println("this is second method for even  no.");
+		
+		
+		for(double i=2;i<=10;i=i+2){
+			
+			System.out.println(i);
+			
+			
+		}
+		
+		
+		
+	}
+
+}
